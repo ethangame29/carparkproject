dc93cde76cee8791cc340300c6e90e362f64a551
20122739 <20122739@tafe.wa.edu.au>
2024-11-07 13:32:40 +0800

    chore: Added instance variables to CarPark Class and added str constructor

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,11 @@
 class CarPark:
-    pass
+
+    def __init__(self, location=None, capacity=None, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors or []
+        self.displays = displays
+
+    def __str__(self):
+        return f"Car Park {self.location} with {self.capacity} capacity"
